c0d6e3b7830ac698101f8e9b878545b682df5e5c
Rico Aprilla Nanda <ricoaprillananda@gmail.com>
2024-11-21 23:55:42 +0700

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        
+        // Smooth scroll to the target element
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+        
+        // Google Analytics Tracking
+        window.dataLayer = window.dataLayer || [];
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+        gtag('config', 'YOUR_TRACKING_ID'); // Replace 'YOUR_TRACKING_ID' with your actual GA tracking ID
+    });
+});
